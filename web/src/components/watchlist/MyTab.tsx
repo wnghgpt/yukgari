147cc834365c8d65970fe1ac1f005c4c6e7124dd
wnghgpt <wnghgpt@gmail.com>
2026-06-14 17:23:45 +0900

    fix: unused useState import 제거

--- web/src/components/watchlist/MyTab.tsx
+++ web/src/components/watchlist/MyTab.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { useAppStore } from '../../store'
 import { usePriceSocket } from '../../hooks/usePriceSocket'
